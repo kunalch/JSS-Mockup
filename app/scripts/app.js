440624bd7ec602295388f5119e700ec27db0183c
kunalch <kunalch@thoughtworks.com>
2013-09-25 15:55:00 +0530

    bed management

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,5 +6,13 @@ angular.module('JSSMockupApp', [])
       .when('/', {templateUrl: 'views/admit.html', controller: 'MainCtrl'})
       .when('/queue', {templateUrl: 'views/queue.html'})
       .when('/bedmanagement', {templateUrl: 'views/bedmanagement.html'})
+      .when('/patientdashboard', {templateUrl: 'views/patientdashboard.html'})
       .otherwise({redirectTo: '/'});
   });
+
+
+$(function(){
+	$(".menu-drag").click(function(){
+		alert("aa");
+	});
+});
